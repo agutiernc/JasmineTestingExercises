3381c58ac4d034d29f3c538295749cb3bceaa21f
agutiernc <agutiernc@gmail.com>
2022-02-23 20:28:23 -0800

    jasmine tests

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,13 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = { amount: 30000, years: 5, rate: 1.2 }
+
+  expect(calculateMonthlyPayment(values)).toEqual('515.40')
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
-});
+  const values = { amount: 650000, years: 30, rate: 1.5 }
 
-/// etc
+  expect(calculateMonthlyPayment(values)).toEqual('2243.28')
+});
